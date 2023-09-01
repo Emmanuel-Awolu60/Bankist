d5fba4d6ff7de1b079f8608a8489cfd7b9165e4a
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-09-01 05:23:18 +0100

    Modified the third section and the javascript file

--- starter/main.js
+++ starter/main.js
@@ -1,21 +1,37 @@
 "use strict";
-
-const openAccountBtn = document.querySelector("#open__account__btn");
+//open account
+const openAccountBtn = document.querySelector(".open__account__btn");
 const openAccount = document.querySelector(".open__account");
 const closeModal = document.querySelector(".btn--close-modal");
 const overlay = document.querySelector(".overlay");
 
-openAccountBtn.addEventListener("click", () => {
+//mobile nav
+const mobileMenu = document.getElementsByClassName("mobile__menu");
+const mobileClose = document.getElementsByClassName("mobile__close");
+const mobileText = document.getElementsByClassName("nav__mobile__text");
+
+// Mobile Section
+// mobileMenu.addEventListener("click", () => {
+//   mobileText;
+// });
+// const removeClass = classList.remove("hidden");
+
+// Open account buttom
+openAccountBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  //Button
   openAccount.classList.remove("hidden");
   overlay.classList.remove("hidden");
 });
 
 closeModal.addEventListener("click", () => {
+  //Close button
   openAccount.classList.add("hidden");
   overlay.classList.add("hidden");
 });
 
 overlay.addEventListener("click", () => {
+  //overlay
   openAccount.classList.add("hidden");
   overlay.classList.add("hidden");
 });
