9613db87f923b98de45e04e68941e08a5b514b8c
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-10-05 22:35:16 +0100

    updated

--- starter/main.js
+++ starter/main.js
@@ -5,6 +5,17 @@ const openAccount = document.querySelector(".openAccount");
 const closeModal = document.querySelector(".btn--close-modal");
 const overlay = document.querySelector(".overlay");
 
+const oprationsBtn = document.querySelector(".oprationsBtn");
+const oprationsBtn1 = document.querySelector(".oprationsBtn--1");
+const oprationsBtn2 = document.querySelector(".oprationsBtn--2");
+// const oprationsBtn3 = document.querySelector(".oprationsBtn--3");
+
+const oprationsBtnSection = document.querySelector(".oprationsBtnSection");
+const oprationsText = document.querySelector(".oprationsText");
+const oprationsText1 = document.getElementsByClassName("oprationsText--1");
+const oprationsText2 = document.getElementsByClassName("oprationsText--2");
+// const oprationsText3 = document.getElementsByClassName("oprationsText--3");
+
 //mobile nav
 const mobileMenu = document.getElementsByClassName("mobileMenu");
 const mobileClose = document.getElementsByClassName("mobileClose");
