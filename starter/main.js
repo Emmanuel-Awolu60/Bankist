37fd22ed126f17ca79907fb283e7e70e92ef16c8
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-10-12 14:27:35 +0100

    updated

--- starter/main.js
+++ starter/main.js
@@ -5,13 +5,13 @@ const openAccount = document.querySelector(".openAccount");
 const closeModal = document.querySelector(".btn--close-modal");
 const overlay = document.querySelector(".overlay");
 
-const oprationsBtn = document.querySelector(".oprationsBtn");
+const taps = document.querySelector(".oprationsBtn");
 const oprationsBtn1 = document.querySelector(".oprationsBtn--1");
 const oprationsBtn2 = document.querySelector(".oprationsBtn--2");
 // const oprationsBtn3 = document.querySelector(".oprationsBtn--3");
 
 const oprationsBtnSection = document.querySelector(".oprationsBtnSection");
-const oprationsText = document.querySelector(".oprationsText");
+const texts = document.querySelector(".oprationsText");
 const oprationsText1 = document.getElementsByClassName("oprationsText--1");
 const oprationsText2 = document.getElementsByClassName("oprationsText--2");
 // const oprationsText3 = document.getElementsByClassName("oprationsText--3");
@@ -45,4 +45,19 @@ overlay.addEventListener("click", () => {
   //overlay
   openAccount.classList.add("hidden");
   overlay.classList.add("hidden");
+});
+
+oprationsBtnSection.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".oprationsBtn");
+
+  if (!clicked) return;
+
+  taps.classList.remove("operations__btn--active");
+  texts.classList.remove("operations__text--active");
+
+  clicked.classList.add("operations__btn--active");
+  texts.classList.add("operation__text--active");
+
+  document.querySelector(`text--${clicked.dataset.texts}`);
+  // .classList.add("operations__text--active");
 });
